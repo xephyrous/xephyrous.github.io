6a4596f67399c5b6245d1aeee57422cf8ef0781c
xephyrous <terminatednull@gmail.com>
2025-03-06 04:48:20 +0000

    Auto-Deployment of Pages [Thu Mar  6 04:48:16 UTC 2025]

---
+++ projects/answers-time/assets/adminpanel-B2aIfTsf.js
@@ -0,0 +1,28 @@
+import{c as i}from"./firebase-D4KhxNe_.js";i();document.querySelector("#app").innerHTML=`
+    <div id="main-window" class="window" style="width: 100%; height: 100%">
+        <div class="title-bar" style="position: relative; z-index: 10">
+            <div class="title-bar-text">Mr Answers Himself!</div>
+            <div class="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Restore"></button>
+                <button aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="window-body">
+            <div class="center-fill">
+                <img id="logo" style="z-index: 20" src="../public/jake_time_banner.png" alt="Log In Banner!">
+            </div>
+            
+            <div class="center-fill">
+                <div class="window" style="pos">
+                    <div class="title-bar" style="position: relative; z-index: 10">
+                        <div class="title-bar-text">Questions for you!</div>
+                    </div>
+                    <div class="window-body">
+                        wsg fuckass
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
---
+++ projects/answers-time/assets/login-BA_gNnKh.js
@@ -0,0 +1,38 @@
+import{l as i}from"./firebase-D4KhxNe_.js";document.querySelector("#app").innerHTML=`
+    <div id="main-window" class="window" style="width: 100%; height: 100%">
+        <div class="title-bar" style="position: relative; z-index: 10">
+            <div class="title-bar-text">Login!</div>
+            <div class="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Restore"></button>
+                <button aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="window-body">
+            <div class="center-fill">
+                <img id="logo" style="z-index: 20" src="../public/log_in.gif" alt="Log In Banner!">
+            </div>
+            
+            <div class="center-fill">
+                <div class="window" style="pos">
+                    <div class="title-bar" style="position: relative; z-index: 10">
+                        <div class="title-bar-text" style="width: 100%; text-align: center">ᓚᘏᗢ</div>
+                    </div>
+                    <div class="window-body">
+                        <div class="field-row-stacked" style="width: 30vw">
+                          <label for="username_field" style="font-weight: bold">Email</label>
+                          <input id="username_field" type="text" style="color: black; font-weight: bold"/>
+                        </div>
+                        <div class="field-row-stacked" style="width: 30vw">
+                          <label for="password_field" style="font-weight: bold">Password</label>
+                          <input id="password_field" type="password" style="color: black; font-weight: bold"/>
+                        </div>
+                        <div class="field-row-stacked" style="width: 30vw">
+                            <button id="login-button" style="color: black; font-weight: bold">Steal My Information!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;document.getElementById("login-button").addEventListener("click",()=>{i(document.getElementById("username_field").value,document.getElementById("password_field").value).then(t=>{t&&(window.location="/projects/answers-time/admin-panel/index.html")})});
