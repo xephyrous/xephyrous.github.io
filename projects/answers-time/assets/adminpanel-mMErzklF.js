ccecd6be0971f957f9ebcf1d2667b17823f7255e
xephyrous <terminatednull@gmail.com>
2025-03-15 04:48:33 +0000

    Auto-Deployment of Pages [Sat Mar 15 04:48:28 UTC 2025]

--- projects/answers-time/assets/adminpanel-mMErzklF.js
+++ projects/answers-time/assets/adminpanel-mMErzklF.js
@@ -10,7 +10,7 @@ import{c as a,g as c}from"./firebase-D-DrtZvu.js";a();document.querySelector("#a
         </div>
         <div class="window-body">
             <div class="center-fill">
-                <img id="logo" style="z-index: 20" src="../public/jake_time_banner.png" alt="Log In Banner!">
+                <img id="logo" style="z-index: 20" src="../jake_time_banner.png" alt="Log In Banner!">
             </div>
             
             <div style="margin-top: 20px; align-items: center; width: 100%; justify-content: center; display: flex">
@@ -25,6 +25,6 @@ import{c as a,g as c}from"./firebase-D-DrtZvu.js";a();document.querySelector("#a
     </div>
     
     <button id="home-button">
-        <img src="../public/icons/home.png" alt="Home Button" style="width: 25px; height: 25px">
+        <img src="../icons/home.png" alt="Home Button" style="width: 25px; height: 25px">
     </button>
 `;document.getElementById("home-button").addEventListener("click",()=>{window.location="/projects/answers-time/index.html"});document.addEventListener("DOMContentLoaded",async()=>{document.getElementById("main-window").style.display="block";const l=document.getElementById("message-window"),s=await c();s.sort((d,t)=>new Date(t.date)-new Date(d.date));for(const d of s){const t=document.createElement("ul");t.classList.add("tree-view");const o=document.createElement("li"),e=document.createElement("div");e.style.width="100%",e.style.display="flex",e.style.flexDirection="row";const i=document.createElement("div");i.style.width="20%",i.innerText=d.date,i.style.fontWeight="bold",i.style.fontSize="12px";const n=document.createElement("div");n.style.width="80%",n.innerText=d.value,n.style.textAlign="left",n.style.fontSize="12px",e.appendChild(i),e.appendChild(n),o.appendChild(e),t.appendChild(o),l.appendChild(t)}});
